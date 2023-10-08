b89d89798ec48ec066ad73da62190b44e236420c
Thomas Wilson <tommyhwilson3@gmail.com>
2023-10-08 15:19:30 -0500

    Add new component rendering

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
+import About from "@/components/About";
 import NavBar from "@/components/NavBar";
-
-const navLinks = [
-  { link: `example.com`, text: "About" },
-  { link: "example2.com", text: "Contact" },
-];
 
 export default function Home() {
   return (
-    <NavBar/>
+    <>
+      <NavBar/>
+      <About/>
+    </>
   );
 }
