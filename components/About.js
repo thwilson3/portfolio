4b53830e5b9c512b62fc0398f89169962b8b9524
Thomas Wilson <tommyhwilson3@gmail.com>
2023-10-09 20:06:37 -0500

    Added text glow and removed elements that made up hamburger menu

--- components/About.js
+++ components/About.js
@@ -1,24 +1,48 @@
 import Image from "next/image";
 
+const buttonList = [
+  {
+    text: "hello",
+    path: "example.com",
+    iconPath: "/email.png",
+    alt: "email icon"
+  },
+  {
+    text: "resume",
+    path: "example2.com",
+    iconPath: "/resume.png",
+    alt: "resume icon"
+  },
+];
 
 export default function About() {
   return (
     <section className="block">
-      <div className="py-12 md:py-16 lg:py-20 mx-auto w-full max-w-7xl px-5 md:px-10">
+      <div className="py-12 bg-background md:py-16 lg:py-20 mx-auto w-full max-w-7xl px-5 md:px-10">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 sm:gap-20">
           <div className="flex-col flex items-start gap-2">
             <div className="">
               <div className="flex-col flex items-start">
                 <div className="flex-col flex items-start gap-2">
-                  <div className="flex grid-cols-2 items-center bg-accent px-3 py-1">
-                    <div className="h-2 w-2 min-w-[8px] mr-2 bg-black rounded-full"></div>
-                    <div className="text-sm text-black sm:text-sm">Houston, Texas</div>
+                  <div className="flex grid-cols-2 items-center bg-accent px-3 py-1 drop-shadow-whiteGlow">
+                    <div className="h-2 w-2 min-w-[8px] mr-2 bg-primary rounded-full"></div>
+                    <div className="text-sm text-black sm:text-sm">
+                      houston, texas
+                    </div>
                   </div>
-                  <p className="flex-col text-secondary text-sm sm:text-xl">Software Engineer</p>
-                  <h1 className="font-thin text-4xl text-primary md:text-6xl mb-6 md:mb-6 lg:mb-8">Tommy Wilson</h1>
-                  <p className="flex-col text-secondary sm:text-xl">Consectetur adipiscing elit duis tristique sollicitudin nibh. Augue mauris augue neque gravida in fermentum. Sapien pellentesque habitant morbi tristique pellentesque.</p>
+                  <p className="flex-col text-secondary text-sm sm:text-xl drop-shadow-whiteGlow">
+                    software engineer
+                  </p>
+                  <h1 className="font-thin text-4xl text-primary md:text-6xl mb-6 md:mb-6 lg:mb-8 drop-shadow-primaryGlow">
+                    tommy wilson
+                  </h1>
+                  <p className="flex-col text-secondary drop-shadow-whiteGlow sm:text-xl">
+                    Consectetur adipiscing elit duis tristique sollicitudin
+                    nibh. Augue mauris augue neque gravida in fermentum. Sapien
+                    pellentesque habitant morbi tristique pellentesque.
+                  </p>
                 </div>
-                <div className="mb-8 mt-8 h-px w-full bg-accent"></div>
+                <div className="mb-8 mt-8 h-px w-full bg-accent drop-shadow-whiteGlow"></div>
                 <div className="flex-col flex items-start gap-2 mb-5 md:mb-6 lg:mb-8">
                   <p className="flex-col text-secondary max-[479px]:text-sm">
                     <strong>2020:</strong> Site of the year in Awwwards.com
@@ -28,28 +52,41 @@ export default function About() {
                   </p>
                 </div>
                 <div className="flex items-center justify-start gap-4 flex-wrap mb-6 md:mb-10 lg:mb-12">
-                  <a href="#" className="flex max-w-full items-center justify-center gap-2.5 p-0 text-center text-sm font-bold uppercase tracking-[0.2px] text-primary">
+                  <a
+                    href="#"
+                    className="flex max-w-full items-center justify-center gap-2.5 p-0 text-center text-sm font-bold uppercase tracking-[0.2px] text-primary"
+                  >
                     <div>All Achievements</div>
-                    <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/64b1465d46adaf3f26099edf_arrow.svg" alt="" className="inline-block" />
+                    <img
+                      src="https://assets.website-files.com/6458c625291a94a195e6cf3a/64b1465d46adaf3f26099edf_arrow.svg"
+                      alt=""
+                      className="inline-block"
+                    />
                   </a>
                 </div>
                 <div className="flex items-center justify-start gap-4 flex-wrap">
-                  <a href="#" className="flex-row flex max-w-full items-center justify-center gap-2 border border-solid border-accent bg-accent px-6 py-3 text-center font-semibold text-black">
-                    <Image src="/email.png" height={25} width={25} alt="email icon" className="inline-block" />
-                    <div>Email Me</div>
-                  </a>
-                  <a href="#" className="flex-row flex max-w-full items-center justify-center gap-2 border border-solid border-accent bg-accent px-6 py-3 text-center font-semibold text-black">
-                    <Image src="/resume.png" height={25} width={25} alt="resume icon" className="inline-block" />
-                    <div>Resume</div>
-                  </a>
+                  {buttonList.map((item, idx) =>
+                    <a
+                      href={item.path}
+                      key={idx}
+                      className="flex-row flex max-w-full items-center justify-center gap-2 border border-solid border-accent bg-accent px-6 py-3 text-center font-semibold text-black drop-shadow-whiteGlow"
+                    >
+                      <Image
+                        src={item.iconPath}
+                        height={25}
+                        width={25}
+                        alt={item.alt}
+                        className="inline-block"
+                      />
+                      <div>{item.text}</div>
+                    </a>
+                  )}
                 </div>
               </div>
             </div>
           </div>
-          <div className="min-h-[530px] overflow-hidden">
-            <Image src="/portrait.png" height={530} width={530} alt="">
-
-            </Image>
+          <div className="min-h-[530px] overflow-hidden rotate-3">
+            <Image src="/portrait.png" height={530} width={530} alt="" />
           </div>
         </div>
       </div>
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,10 @@
 "use client";
 import React, { useState } from "react";
+import HamburgerMenu from "./HamburgerMenu";
 
 const navItems = [
-  { link: `example.com`, text: "ABOUT" },
-  { link: "example2.com", text: "CONTACT" },
+  { link: `example.com`, text: "about" },
+  { link: "example2.com", text: "contact" },
 ];
 
 export default function Navbar() {
@@ -15,8 +16,8 @@ export default function Navbar() {
 
   return (
     <section>
-      <div className="h-auto w-screen bg-black py-2 text-white border-b-[1px_solid_rgb(91,103,130)]">
-        <nav className="font-inter mx-auto h-auto w-full max-w-[1600px] lg:relative lg:top-0">
+      <div className="h-auto w-screen bg-background py-2 text-white">
+        <nav className="mx-auto h-auto w-full max-w-[1600px] lg:relative lg:top-0">
           <div className="flex flex-col px-6 py-6 lg:flex-row lg:items-center lg:justify-between lg:px-10 lg:py-4 xl:px-20">
             <a href="#">
               <div>thwilson3</div>
@@ -28,43 +29,18 @@ export default function Navbar() {
             >
               {navItems.map((item, idx) => (
                 <div key={idx} className="flex items-center font-semibold text-white">
-                  <span className="mr-1">00{idx + 1}:</span>
+                  <span className="mr-1 drop-shadow-whiteGlow">00{idx + 1}:</span>
                   <a
                     href={item.link}
                     key={idx}
-                    className="ml-1 bg-transparent p-1 font-thin text-primary transition duration-500 ease-in-out hover:text-black hover:bg-primary max-[991px]:block md:px-10 lg:px-4"
+                    className="ml-1 bg-primary rounded-full drop-shadow-primaryGlow p-1 outline outline-1 font-thin text-background transition duration-500 ease-in-out hover:text-black hover:drop-shadow-whiteGlow hover:bg-accent max-[991px]:block md:px-10 lg:px-4"
                   >
                     {item.text}
                   </a>
                 </div>
               ))}
             </div>
-            <a
-              href="#"
-              className="absolute right-5 lg:hidden"
-              onClick={toggleMenu}
-            >
-              <svg
-                width="1.25rem"
-                height="1rem"
-                viewBox="0 0 20 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M19 7H1C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9H19C19.5523 9 20 8.55228 20 8C20 7.44772 19.5523 7 19 7Z"
-                  fill="currentColor"
-                ></path>
-                <path
-                  d="M19 0H7C6.44772 0 6 0.447715 6 1C6 1.55228 6.44772 2 7 2H19C19.5523 2 20 1.55228 20 1C20 0.447715 19.5523 0 19 0Z"
-                  fill="currentColor"
-                ></path>
-                <path
-                  d="M19 14H11C10.4477 14 10 14.4477 10 15C10 15.5523 10.4477 16 11 16H19C19.5523 16 20 15.5523 20 15C20 14.4477 19.5523 14 19 14Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </a>
+            <HamburgerMenu toggleMenu={toggleMenu}/>
           </div>
         </nav>
       </div>
